4952e27c249a3b8c20b1c7ab97fb77fa478ed0eb
Weronika <weronikalimberger@gmail.com>
2023-07-14 11:00:07 +0100

    Skeleton of the game where the current rounds are hard coded to 3 and user input is not yet considered.

--- src/components/TinTonGame.js
+++ src/components/TinTonGame.js
@@ -5,52 +5,93 @@ const green = '#29B64C';
 const red = '#E82A68';
 const yellow = '#EAC606';
 
-const TinTonGame = () => {
-  const [sequence, setSequence] = useState([]);
-  const [playerSequence, setPlayerSequence] = useState([]);
-  const [round, setRound] = useState(1);
-  const [message, setMessage] = useState('');
+const Game = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [buttonColor, setButtonColor] = useState([blue, green, red, yellow]);
+  const [randomButtonIndex, setRandomButtonIndex] = useState(null);
+  const [sequence, setSequence] = useState([]);
+  const [round, setRound] = useState(1);
+  const [playerSequence, setPlayerSequence] = useState([]);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
+    let timer;
     if (isPlaying) {
-      generateSequence();
+      if (round > 3) {
+        setIsPlaying(false);
+      } else if (randomButtonIndex !== null) {
+        timer = setTimeout(() => {
+          setButtonColor((prevColors) => {
+            const updatedColors = [...prevColors];
+            updatedColors[randomButtonIndex] = getColorByIndex(randomButtonIndex);
+            return updatedColors;
+          });
+          setRandomButtonIndex(null);
+          setRound((prevRound) => prevRound + 1);
+        }, 500);
+      } else {
+        timer = setTimeout(() => {
+          const newRandomButtonIndex = Math.floor(Math.random() * 4);
+          setButtonColor((prevColors) => {
+            const updatedColors = [...prevColors];
+            updatedColors[newRandomButtonIndex] = 'black';
+            return updatedColors;
+          });
+          setSequence((prevNumbers) => [...prevNumbers, newRandomButtonIndex]);
+          setRandomButtonIndex(newRandomButtonIndex);
+        }, 2000);
+      }
     }
-  }, [isPlaying]);
+    return () => clearTimeout(timer);
+  }, [isPlaying, round, randomButtonIndex]);
 
-  const generateSequence = () => {
-    const nextButton = Math.floor(Math.random() * 4);
-    setSequence(prevSequence => [...prevSequence, nextButton]);
+  const getColorByIndex = (index) => {
+    switch (index) {
+      case 0:
+        return blue;
+      case 1:
+        return green;
+      case 2:
+        return red;
+      case 3:
+        return yellow;
+      default:
+        return '';
+    }
   };
 
   const startGame = () => {
-    setSequence([]);
-    setPlayerSequence([]);
-    setRound(1);
-    setMessage('');
     setIsPlaying(true);
+    const randomNumber = Math.floor(Math.random() * 4);
+    const updatedButtonColor = [blue, green, red, yellow];
+    updatedButtonColor[randomNumber] = 'black';
+    setButtonColor(updatedButtonColor);
+    setSequence([randomNumber]);
+    setRandomButtonIndex(randomNumber);
+    setRound(1);
   };
 
-  const handleButtonClick = buttonIndex => {
-    if (isPlaying) {
-      const updatedPlayerSequence = [...playerSequence, buttonIndex];
-      setPlayerSequence(updatedPlayerSequence);
-
-      if (!checkSequence(updatedPlayerSequence)) {
-        setIsPlaying(false);
-        setMessage('Game Over! You clicked the wrong button.');
-      } else if (updatedPlayerSequence.length === sequence.length) {
-        setPlayerSequence([]);
-        setRound(prevRound => prevRound + 1);
-        setMessage(`Round ${round + 1}`);
-        setTimeout(() => {
-          generateSequence();
-        }, 1000);
-      }
+  const handleButtonClick = () => {
+    if (isPlaying && round <= 3) {
+      // Add your logic here for handling button clicks during the game
+      // You can use the sequence state to check the correct sequence
+      // and update the game state accordingly
     }
   };
 
-  const checkSequence = playerSequence => {
+  const startNextRound = () => {
+    const newRandomButtonIndex = Math.floor(Math.random() * 4);
+    setButtonColor((prevColors) => {
+      const updatedColors = [...prevColors];
+      updatedColors[newRandomButtonIndex] = 'black';
+      return updatedColors;
+    });
+    setSequence((prevNumbers) => [...prevNumbers, newRandomButtonIndex]);
+    setRandomButtonIndex(newRandomButtonIndex);
+  };
+  
+
+  const checkSequence = (playerSequence) => {
     for (let i = 0; i < playerSequence.length; i++) {
       if (playerSequence[i] !== sequence[i]) {
         return false;
@@ -58,48 +99,48 @@ const TinTonGame = () => {
     }
     return true;
   };
+ 
 
   return (
     <div>
-      <h1>TinTon</h1>
-      <p>Sequence Game</p>
+      <h1>Color Game</h1>
       <div className="game-board">
         <button
-          style={{ backgroundColor: blue }}
-          className={`color-button ${playerSequence.includes(0) ? 'black' : ''}`}
-          onClick={() => handleButtonClick(0)}
-          disabled={!isPlaying}
+          style={{ backgroundColor: buttonColor[0] }}
+          className="color-button"
+          disabled={isPlaying}
+          onClick={handleButtonClick}
         ></button>
         <button
-          style={{ backgroundColor: green }}
-          className={`color-button ${playerSequence.includes(1) ? 'black' : ''}`}
-          onClick={() => handleButtonClick(1)}
-          disabled={!isPlaying}
+          style={{ backgroundColor: buttonColor[1] }}
+          className="color-button"
+          disabled={isPlaying}
+          onClick={handleButtonClick}
         ></button>
-        <span className='divide'></span>
+        <span className="divide"></span>
         <button
-          style={{ backgroundColor: red }}
-          className={`color-button ${playerSequence.includes(2) ? 'black' : ''}`}
-          onClick={() => handleButtonClick(2)}
-          disabled={!isPlaying}
+          style={{ backgroundColor: buttonColor[2] }}
+          className="color-button"
+          disabled={isPlaying}
+          onClick={handleButtonClick}
         ></button>
         <button
-          style={{ backgroundColor: yellow }}
-          className={`color-button ${playerSequence.includes(3) ? 'black' : ''}`}
-          onClick={() => handleButtonClick(3)}
-          disabled={!isPlaying}
+          style={{ backgroundColor: buttonColor[3] }}
+          className="color-button"
+          disabled={isPlaying}
+          onClick={handleButtonClick}
         ></button>
       </div>
       <div className="game-info">
-        <p>{message}</p>
-        {!isPlaying && (
+        {!isPlaying && round <= 3 && (
           <button className="start-button" onClick={startGame}>
             Start
           </button>
         )}
+        {!isPlaying && round > 3 && <p>Your Turn</p>}
       </div>
     </div>
   );
 };
 
-export default TinTonGame;
+export default Game;
