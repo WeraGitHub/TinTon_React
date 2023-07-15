80a086a08f854152dc6ea2f25f4393a888dbaed2
Weronika <weronikalimberger@gmail.com>
2023-07-15 21:44:32 +0100

    Use createRoot to render your app instead of ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+import { createRoot } from "react-dom/client";
 import './index.css';
+import TinTonGame from './components/TinTonGame';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// Use createRoot to render your app instead of ReactDOM.render()
+// because ReactDOM.render is no longer supported in React 18.
+const root = document.getElementById("root");
+createRoot(root).render(<TinTonGame />);
