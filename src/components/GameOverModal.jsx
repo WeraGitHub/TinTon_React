e4f0f78da0859bd8b4bbd7412bc4e5c0c3181984
Weronika <weronikalimberger@gmail.com>
2023-07-17 20:13:07 +0100

    MAke game over purple

--- src/components/GameOverModal.jsx
+++ src/components/GameOverModal.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./GameOverModal.css";
 
 const GameOverModal = ({ onClick, name, score }) => (
-  <div className="modal-mask">
-    <div className="modal">
+  <div className="game-over-modal-mask">
+    <div className="game-over-modal">
       <div className="message-area">
          <p>Game Over {name}!</p>
          <p className="small-p">You clicked on the wrong button silly!</p>
          <p>Your score: {score}</p>
          <p>🐶</p>      
       </div>
-      <button className="start-button" onClick={onClick}>
+      <button className="game-over-start-button" onClick={onClick}>
         Start again?
       </button>
     </div>
